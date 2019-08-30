864b2c9c3c6a622e686b2d71f982d99d310d0b28
Austin Smith <trunk9slamchest@gmail.com>
2019-08-30 02:56:10 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,29 +1,29 @@
-# My Code here....
+  # My Code here....
 
 def map_to_negativize(array)
-  new_arr = array.map { |e| e * -1 }
-  p new_arr
+    new_arr = array.map { |e| e * -1 }
+    p new_arr
 end
 
 # map_to_negativize([1, 2, 3, -9])
 
 def map_to_no_change(array)
-  new_arr = array.map { |e| e }
-  p new_arr
+    new_arr = array.map { |e| e }
+    p new_arr
 end
 
 # map_to_no_change(["paul", "gurney", "vladimir", "jessica", "chani"])
 
 def map_to_double(array)
-  new_arr = array.map { |e| e * 2 }
-  p new_arr
+    new_arr = array.map { |e| e * 2 }
+    p new_arr
 end
 
 # map_to_double([1, 2, 3, -9])
 
 def map_to_square(array)
-  new_arr = array.map { |e| e * e }
-  p new_arr
+    new_arr = array.map { |e| e * e }
+    p new_arr
 end
 
 # map_to_square([1, 2, 3, -9])
